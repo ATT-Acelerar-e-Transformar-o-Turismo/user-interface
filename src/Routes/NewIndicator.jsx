2bd9bdd9d33cf0cf466892fa3ab4008098879655
Ric <henriqueft04@ua.pt>
2025-03-12 12:04:15 +0000

    add new indicator forms completed

--- src/Routes/NewIndicator.jsx
+++ src/Routes/NewIndicator.jsx
@@ -44,10 +44,6 @@ export default function NewIndicator() {
                             <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                         </div>
 
-                        <div>
-                            <label htmlFor="small-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Periodicity</label>
-                            <input type="text" id="small-input" className="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
-                        </div>
                     </form>
                     <div className='flex justify-end w-full mt-4'>                    
                         <AddDataDropdown />
