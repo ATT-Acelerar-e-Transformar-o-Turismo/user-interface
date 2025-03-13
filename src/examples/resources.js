d59452bed38342bcdb0234085a15cb66d0d269e7
Sebastião Teixeira <steixeira2004@gmail.com>
2025-03-13 00:43:13 +0000

    Add resource management pages

---
+++ src/examples/resources.js
@@ -0,0 +1,12 @@
+const exampleResources = [
+  { id: 1, name: 'gdp_growth_rate.csv', 'start period': '2023-01-01', 'end period': '2023-06-30', indicator: 1 },
+  { id: 2, name: 'gdp_growth_rate.xls', 'start period': '2023-07-01', 'end period': '2023-12-31', indicator: 1 },
+  { id: 3, name: 'unemployment_rate.csv', 'start period': '2023-01-01', 'end period': '2023-03-31', indicator: 2 },
+  { id: 4, name: 'unemployment_rate.xls', 'start period': '2023-04-01', 'end period': '2023-06-30', indicator: 2 },
+  { id: 5, name: 'literacy_rate.csv', 'start period': '2023-01-01', 'end period': '2023-04-30', indicator: 3 },
+  { id: 6, name: 'literacy_rate.xls', 'start period': '2023-05-01', 'end period': '2023-08-31', indicator: 3 },
+  { id: 7, name: 'carbon_emissions.csv', 'start period': '2023-01-01', 'end period': '2023-12-31', indicator: 4 },
+  { id: 8, name: 'carbon_emissions.xls', 'start period': '2024-01-01', 'end period': '2024-12-31', indicator: 4 },
+];
+
+export default exampleResources;
